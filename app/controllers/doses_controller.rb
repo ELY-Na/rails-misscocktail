3e8f915e6dcb18369217f80f3a7bb7dc7e45eb46
Najia EL YOUZGHI <najia.elyouzghi@gmail.com>
2019-12-05 18:34:29 +0100

    pas finit

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,14 +1,27 @@
 class DosesController < ApplicationController
+  before_action :set_dose, only: [:show, :update, :destroy]
   def new
     @dose = Dose.new
   end
 
   def create
-    @cocktail = Cocktail.find(params[:id])
-    @dose = Dose.find(params[:id])
-    @dose.cocktail = @dose
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose = Dose.new(dose_params)
+    @dose.cocktail = @cocktail
     @dose.save
+    render :show
   end
+
   def show
+  end
+
+  private
+
+  def dose_params
+    params.require(:dose).permit(:description, :ingredient_id)
+  end
+
+  def set_dose
+    @dose = Dose.find(params[:id])
   end
 end
--- app/models/dose.rb
+++ app/models/dose.rb
@@ -2,8 +2,7 @@ class Dose < ApplicationRecord
   belongs_to :cocktail
   belongs_to :ingredient
 
-  validates :description, presence: true
-  validates :ingredient, uniqueness: { scope: :cocktail }
-  # validates :cocktail, uniqueness: { scope: :ingredient }
+  # validates :description, presence: true
+  validates :cocktail, uniqueness: { scope: :ingredient }
   # l'un ou l'autre fonctionne au rake à voir la diff
 end
